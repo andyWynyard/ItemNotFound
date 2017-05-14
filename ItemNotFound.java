66127d7b1605f37afedfd1cc76c20261b55cc516
Andrew Wynyard <andrew.wynyard@gmail.com>
2017-05-14 22:15:27 +0200

    cleaned up code

--- ItemNotFound.java
+++ ItemNotFound.java
@@ -10,38 +10,39 @@ public class ItemNotFound {
 			"Eunostus", "Ganymede", "Hector", "Iolaus", 
 			"Jason", "Meleager", "Odysseus", "Orpheus", 
 			"Perseus", "Thesius", "Achilles", "Achilles"};
-			String guess;
-			boolean found;
-			int counter = 0;
+		String guess;
+		boolean found;
+		int counter = 0;
 
-			System.out.println("Pop Quiz!");
-			System.out.print("Name any *mortal* hero from" + 
-				"Greek mythology: ");
-			guess = keyboard.next();
+		System.out.println("Pop Quiz!");
+		System.out.print("Name any *mortal* hero from" + 
+			"Greek mythology: ");
+		guess = keyboard.next();
 
-			found = false;
+		found = false;
 
-			//for (String hero: heroes) {
-			//	if (guess.equals(hero)) {
-			//		counter++;
-			//	}
-			//}
-
-			for (String hero: heroes) {
-				if (guess.equals(hero)) {					
-					found = true;
-					counter++; 	//Moved counter here - simpler
-				}				//and smaller code than req
-			}					//in the study drill.
-
-			if (found == false) {
-				System.out.println("No, " + guess + 
-					" wasn't a Greek mortal hero.");
-			} else {
-				System.out.println("Yes " + guess + 
-						" was a Greek mortal hero. He was " +
-						"found " + counter + " times.");
+		/*
+		for (String hero: heroes) {
+			if (guess.equals(hero)) {
+				counter++;
 			}
+		}
+		*/
 
+		for (String hero: heroes) {
+			if (guess.equals(hero)) {					
+				found = true;
+				counter++; 	//Moved counter here - simpler
+			}				//and smaller code than req
+		}					//in the study drill.
+
+		if (found == false) {
+			System.out.println("No, " + guess + 
+				" wasn't a Greek mortal hero.");
+		} else {
+			System.out.println("Yes " + guess + 
+					" was a Greek mortal hero. He was " +
+					"found " + counter + " times.");
+		}
 	}
 }
